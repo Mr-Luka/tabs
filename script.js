67dcd2a4a8a3e09d25c168e3cda56f42fafba789
Luka <luka.luka.luka93@gmail.com>
2024-05-30 13:40:29 -0700

    different way

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const tabs = document.querySelector(".tabs");
 const tabButtons = tabs.querySelectorAll(`[role="tab"]`);
-const tabPanels = tabs.querySelectorAll(`[role="tabpanel"]`);
+const tabPanels = Array.from(tabs.querySelectorAll(`[role="tabpanel"]`));
 
 function handleTabClick(e) {
     tabPanels.forEach(panel=>{
@@ -12,8 +12,13 @@ function handleTabClick(e) {
     })
     e.currentTarget.setAttribute('aria-selected', true)
     const {id} = e.currentTarget;
-    const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
-    tabPanel.hidden = false;
+    // const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+    // tabPanel.hidden = false;
+    tabPanels.find(panel=> {
+        if(panel.getAttribute(`aria-labeledby`) === id) {
+            return true;
+        } 
+    })
 }
 
 tabButtons.forEach(button=> button.addEventListener("click", handleTabClick));
